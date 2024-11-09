870ff360139f7d2fa06ac395292963e5dfc479aa
Robert Ghafoor <robert.ghafoor@nordicmorning.com>
2024-11-09 13:21:23 +0100

    sort drag added

--- src/key-value-tags-editor.element.ts
+++ src/key-value-tags-editor.element.ts
@@ -1,7 +1,9 @@
-import { css, customElement, html, property } from '@umbraco-cms/backoffice/external/lit';
+import { css, customElement, html, property, state } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 import { UmbPropertyValueChangeEvent } from '@umbraco-cms/backoffice/property-editor';
 import type { UmbPropertyEditorUiElement } from '@umbraco-cms/backoffice/extension-registry';
+import { UmbSorterController, UmbSorterConfig } from '@umbraco-cms/backoffice/sorter';
+import { repeat } from '@umbraco-cms/backoffice/external/lit';
 import type { UmbTagsInputElement } from '@umbraco-cms/backoffice/tags';
 import '@umbraco-cms/backoffice/tags';
 
@@ -16,100 +18,147 @@ export class KeyValueTagsEditorElement extends UmbLitElement implements UmbPrope
     @property({ type: Array })
     public value?: Array<KeyValueTagItem>;
 
-    @property({ type: Boolean, reflect: true })
-    readonly = false;
+    @property({ type: Object })
+    public config: any;
 
-    @property()
-    public config?: any;
+    @state()
+    private _items: Array<KeyValueTagItem> = [];
+
+    private static generateUniqueId(item: KeyValueTagItem): string {
+        const title = item.title.toLowerCase().replace(/[^a-z0-9]/g, '');
+        const key = item.key.toLowerCase().replace(/[^a-z0-9]/g, '');
+        return `${title}_${key}`;
+    }
+
+    private static SORTER_CONFIG: UmbSorterConfig<KeyValueTagItem, HTMLElement> = {
+        itemSelector: '.key-value-tags__item',
+        containerSelector: '.key-value-tags',
+        getUniqueOfElement: (element) => {
+            const id = element.getAttribute('data-item-id');
+            return id || '';
+        },
+        getUniqueOfModel: (item) => {
+            return KeyValueTagsEditorElement.generateUniqueId(item);
+        }
+    };
+
+    #sorter = new UmbSorterController<KeyValueTagItem, HTMLElement>(this, {
+        ...KeyValueTagsEditorElement.SORTER_CONFIG,
+        onChange: ({ model }) => {
+            const oldValue = this._items;
+            this._items = [...model];
+            this.requestUpdate('_items', oldValue);
+            this.dispatchChange();
+        },
+    });
+
+    @property({ type: Boolean, reflect: true })
+    public get readonly() {
+        return this.#readonly;
+    }
+    public set readonly(value: boolean) {
+        this.#readonly = value;
+        if (this.#readonly) {
+            this.#sorter.disable();
+        } else {
+            this.#sorter.enable();
+        }
+    }
+    #readonly = false;
 
     constructor() {
         super();
         this.value = [];
     }
 
-    #onAdd() {
-        this.value = [...(this.value ?? []), { title: '', key: '', tags: [] }];
-        this.dispatchEvent(new UmbPropertyValueChangeEvent());
+    connectedCallback() {
+        super.connectedCallback();
+        if (this.value && this.value.length > 0) {
+            this._items = [...this.value];
+            this.#sorter.setModel(this._items);
+        }
     }
 
-    #onRemove(index: number) {
-        this.value = (this.value ?? []).filter((_, i) => i !== index);
-        this.dispatchEvent(new UmbPropertyValueChangeEvent());
-    }
-
-    #onKeyChange(index: number, event: Event) {
-        const input = event.target as HTMLInputElement;
-        this.value = (this.value ?? []).map((item, i) => 
-            i === index ? { ...item, key: input.value } : item
-        );
-        this.dispatchEvent(new UmbPropertyValueChangeEvent());
-    }
-
-    #onTagsChange(index: number, event: CustomEvent) {
-        const tags = ((event.target as UmbTagsInputElement).value as string)
-            .split(',')
-            .filter(tag => tag.trim() !== '');
+    willUpdate(changedProperties: Map<string, any>) {
+        super.willUpdate(changedProperties);
         
-        this.value = (this.value ?? []).map((item, i) => 
-            i === index ? { ...item, tags } : item
-        );
-        this.dispatchEvent(new UmbPropertyValueChangeEvent());
+        if (changedProperties.has('value') && this.value) {
+            this._items = [...this.value];
+            this.#sorter.setModel(this._items);
+        }
     }
 
-    #onTitleChange(index: number, event: Event) {
-        const input = event.target as HTMLInputElement;
-        this.value = (this.value ?? []).map((item, i) => 
-            i === index ? { ...item, title: input.value } : item
-        );
+    private dispatchChange() {
+        this.value = this._items;
         this.dispatchEvent(new UmbPropertyValueChangeEvent());
     }
 
     render() {
         return html`
             <div class="key-value-tags">
-                ${(this.value ?? []).map((item, index) => html`
-                    <div class="key-value-tags__item">
-                        <div class="key-value-tags__item-header">
-                            <div class="key-value-tags__item-inputs">
-                                <uui-input
-                                    .value=${item.title}
-                                    placeholder="Enter title"
-                                    ?readonly=${this.readonly}
-                                    @change=${(e: Event) => this.#onTitleChange(index, e)}
-                                    style="min-height: 40px; padding: var(--uui-tag-padding, var(--uui-size-space-1, 3px) calc(var(--uui-size-space-1, 3px) + 0.5em)); padding-left: 0;">
-                                </uui-input>
+                ${repeat(
+                    this._items,
+                    (item) => KeyValueTagsEditorElement.generateUniqueId(item),
+                    (item, index) => {
+                        const itemId = KeyValueTagsEditorElement.generateUniqueId(item);
+                        return html`
+                            <div 
+                                class="key-value-tags__item"
+                                id="${itemId}"
+                                data-item-id="${itemId}"
+                                ?disabled=${this.readonly}>
+                                <div class="key-value-tags__item-header">
+                                    <uui-button 
+                                        class="drag-handle"
+                                        label="Drag to reorder"
+                                        tabindex="-1"
+                                        style="min-height: 40px; min-width: 40px;"
+                                        compact>
+                                        <uui-icon name="icon-navigation"></uui-icon>
+                                    </uui-button>
 
-                                <uui-input
-                                    .value=${item.key}
-                                    placeholder="Enter key"
+                                    <div class="key-value-tags__item-inputs">
+                                        <uui-input
+                                            .value=${item.title}
+                                            placeholder="Enter title"
+                                            ?readonly=${this.readonly}
+                                            @change=${(e: Event) => this.#onTitleChange(index, e)}
+                                            style="min-height: 40px; padding: var(--uui-tag-padding, var(--uui-size-space-1, 3px) calc(var(--uui-size-space-1, 3px) + 0.5em)); padding-left: 0;">
+                                        </uui-input>
+
+                                        <uui-input
+                                            .value=${item.key}
+                                            placeholder="Enter key"
+                                            ?readonly=${this.readonly}
+                                            @change=${(e: Event) => this.#onKeyChange(index, e)}
+                                            style="min-height: 40px; padding: var(--uui-tag-padding, var(--uui-size-space-1, 3px) calc(var(--uui-size-space-1, 3px) + 0.5em)); padding-left: 0;">
+                                        </uui-input>
+                                    </div>
+
+                                    ${!this.readonly ? html`
+                                        <uui-button
+                                            compact
+                                            look="secondary"
+                                            color="danger"
+                                            label="Remove"
+                                            @click=${() => this.#onRemove(index)}
+                                            style="min-height: 40px; min-width: 55.0278px;">
+                                            <uui-icon name="icon-trash"></uui-icon>
+                                        </uui-button>
+                                    ` : ''}
+                                </div>
+                                
+                                <umb-tags-input
+                                    .value=${item.tags.join(',')}
+                                    .items=${item.tags}
+                                    .config=${this.config}
                                     ?readonly=${this.readonly}
-                                    @change=${(e: Event) => this.#onKeyChange(index, e)}
-                                    style="min-height: 40px; padding: var(--uui-tag-padding, var(--uui-size-space-1, 3px) calc(var(--uui-size-space-1, 3px) + 0.5em)); padding-left: 0;">
-                                </uui-input>
+                                    @change=${(e: CustomEvent) => this.#onTagsChange(index, e)}>
+                                </umb-tags-input>
                             </div>
-
-                            ${!this.readonly ? html`
-                                <uui-button
-                                    compact
-                                    look="secondary"
-                                    color="danger"
-                                    label="Remove"
-                                    @click=${() => this.#onRemove(index)}
-                                    style="min-height: 40px; min-width: 55.0278px;">
-                                    <uui-icon name="icon-trash"></uui-icon>
-                                </uui-button>
-                            ` : ''}
-                        </div>
-                        
-                        <umb-tags-input
-                            .value=${item.tags.join(',')}
-                            .items=${item.tags}
-                            .config=${this.config}
-                            ?readonly=${this.readonly}
-                            @change=${(e: CustomEvent) => this.#onTagsChange(index, e)}>
-                        </umb-tags-input>
-                    </div>
-                `)}
+                        `;
+                    }
+                )}
 
                 ${!this.readonly ? html`
                     <div class="add-item-container">
@@ -191,8 +240,90 @@ export class KeyValueTagsEditorElement extends UmbLitElement implements UmbPrope
                 width: 100%;
                 border-left: 2px solid var(--uui-color-border);
             }
+
+            /* Draggable styles */
+            .draggable-mirror {
+                background: var(--uui-color-surface);
+                border: 1px solid var(--uui-color-border-emphasis);
+                border-radius: var(--uui-border-radius);
+                padding: var(--uui-size-space-4);
+                box-shadow: var(--uui-shadow-depth-3);
+                opacity: 0.9;
+            }
+
+            .draggable-source--is-dragging {
+                opacity: 0.3;
+                border: 1px dashed var(--uui-color-border);
+            }
+
+            /* Drag handle */
+            .drag-handle {
+                cursor: move;
+                color: var(--uui-color-text-alt);
+                width: var(--uui-button-height);
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+
+            .drag-handle:hover {
+                color: var(--uui-color-text);
+                background: var(--uui-color-surface-emphasis);
+                border-color: var(--uui-color-border-emphasis);
+            }
+
+            .key-value-tags__item-header {
+                display: grid;
+                grid-template-columns: auto 1fr auto;
+                gap: var(--uui-size-space-3);
+                align-items: center;
+            }
         `
     ];
+
+    #onAdd() {
+        const newItems = [
+            ...this._items,
+            { title: '', key: '', tags: [] }
+        ];
+        this._items = newItems;
+        this.#sorter.setModel(newItems);
+        this.dispatchChange();
+    }
+
+    #onRemove(index: number) {
+        const newItems = this._items.filter((_, i) => i !== index);
+        this._items = newItems;
+        this.#sorter.setModel(newItems);
+        this.dispatchChange();
+    }
+
+    #onTitleChange(index: number, event: Event) {
+        const input = event.target as HTMLInputElement;
+        this._items = this._items.map((item, i) => 
+            i === index ? { ...item, title: input.value } : item
+        );
+        this.dispatchChange();
+    }
+
+    #onKeyChange(index: number, event: Event) {
+        const input = event.target as HTMLInputElement;
+        this._items = this._items.map((item, i) => 
+            i === index ? { ...item, key: input.value } : item
+        );
+        this.dispatchChange();
+    }
+
+    #onTagsChange(index: number, event: CustomEvent) {
+        const tags = ((event.target as UmbTagsInputElement).value as string)
+            .split(',')
+            .filter(tag => tag.trim() !== '');
+        
+        this._items = this._items.map((item, i) => 
+            i === index ? { ...item, tags } : item
+        );
+        this.dispatchChange();
+    }
 }
 
 export default KeyValueTagsEditorElement;
